2515f17a9002e1af8739f97f3249167164f0e384
Li Ling <liling@kth.se>
2022-04-11 17:45:59 +0200

    Compute flatten pixel array #TODO: fix the issues when the keypoint pixel is at the edge, which results in the final descriptor having different length...

--- src/keypoints.py
+++ src/keypoints.py
@@ -43,3 +43,54 @@ def compute_descriptors_at_annotated_locations(patch: SSSPatch, algo: cv2.Featur
             0, 255, cv2.NORM_MINMAX).astype('uint8')
     annotated_kps, desc = algo.compute(normalized_8bit_img, annotated_kps)
     return annotated_kps, desc
+
+def compute_flattened_neighbourhood_pixel_values(patch: SSSPatch, ping_neighbour: int = 8,
+        bin_neighbour: int = 8, use_orig_sss_intensities: bool = False):
+    """For all annotated keypoints in a given SSSPatch, compute a descriptor in the form of flatten
+    pixel values around the keypoint location.
+
+    Parameters
+    ----------
+    patch: SSSPatch
+        The annotated keypoints from this patch will be used for descriptor computations.
+    ping_neighbour: int = 8
+        The number of pings prior and after the keypoint ping to be counted into its
+        "neighbourhood". A ping_neighbour of 8 means that the descriptor is computed from the 8
+        pings proceeding the keypoint ping +  the keypoint ping + the 8 pings after keypoint ping =
+        8 + 1 + 8 = 17 pings.
+    bin_neighbour: int = 8
+        The number of bins prior and after the keypoint bin to be counted into its "neighbourhood".
+        Similar to ping_neighbour.
+    use_orig_sss_intensities: bool = False
+        If False, use the pixel values after applying utils.normalize_waterfall_image to
+        sss_waterfall_image. If True, use the pixel values from sss_waterfall_image directly.
+
+    Returns
+    -------
+    annotated_kps: list[(int, int)]
+        The list consist of tuples of the form (bin_nbr (x value), ping_nbr (y value)).
+    desc: np.array
+        The descriptors computed by flattening the pixel array defined by ping_neighbour and
+        bin_neighbour. Shape = (nbr_kps, (ping_neighbour*2 + 1) * (bin_neighbour*2 + 1)).
+    """
+    img = patch.sss_waterfall_image
+    if not use_orig_sss_intensities:
+        img = normalize_waterfall_image(img)
+
+    annotated_kps = []
+    desc = []
+    for kp in patch.annotated_keypoints.values():
+        ping_nbr, bin_nbr = kp['pos'][0], kp['pos'][1]
+        annotated_kps.append((bin_nbr, ping_nbr))
+
+
+        #TODO: debug code when the keypoint is at the edge
+        min_ping_nbr = max(ping_nbr - ping_neighbour, 0)
+        max_ping_nbr = min(ping_nbr + ping_neighbour + 1, img.shape[0])
+        if min_ping_nbr == 0:
+            pass
+
+        flatten_pixels = img[ping_nbr - ping_neighbour:ping_nbr + ping_neighbour + 1, bin_nbr-bin_neighbour:bin_nbr+bin_neighbour+1].flatten()
+        desc.append(flatten_pixels)
+        print(ping_nbr, bin_nbr, len(flatten_pixels))
+    return annotated_kps, desc #np.array(desc)
