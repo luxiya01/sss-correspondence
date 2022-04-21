cace3e246d6b3f023bdb08d23f57259bbcee43e7
Li Ling <liling@kth.se>
2022-04-21 18:01:07 +0200

    Modify kp structure and add function to compute kp for all patches

--- src/keypoints.py
+++ src/keypoints.py
@@ -1,11 +1,47 @@
-import cv2
-import numpy as np
+import os
+import pickle
 from matplotlib import pyplot as plt
-from utils import normalize_waterfall_image
+import numpy as np
+import cv2
+from utils import normalize_waterfall_image, get_sorted_patches_list
 from sss_patches import SSSPatch
 
-def compute_descriptors_at_annotated_locations(patch: SSSPatch, algo: cv2.Feature2D, kp_size: int =
-        16, use_orig_sss_intensities: bool=False):
+
+def compute_and_store_descriptors(folder: str, algo: str = 'sift'):
+    patches = get_sorted_patches_list(folder)
+    sift = cv2.SIFT_create()
+
+    outfolder = os.path.join(folder, algo)
+    if not os.path.exists(outfolder):
+        os.makedirs(outfolder)
+
+    for patch_path in patches:
+        with open(patch_path, 'rb') as f:
+            patch = pickle.load(f)
+
+            if algo == 'sift':
+                kp_raw, desc_raw = compute_descriptors_at_annotated_locations(
+                    patch, sift, use_orig_sss_intensities=True)
+                kp_norm, desc_norm = compute_descriptors_at_annotated_locations(
+                    patch, sift, use_orig_sss_intensities=False)
+            elif algo == 'neighbour':
+                kp_raw, desc_raw = compute_flattened_neighbourhood_pixel_values(
+                    patch, use_orig_sss_intensities=True)
+                kp_norm, desc_norm = compute_flattened_neighbourhood_pixel_values(
+                    patch, use_orig_sss_intensities=False)
+
+            np.savez(os.path.join(outfolder, f'patch{patch.patch_id}'),
+                     kp_raw=kp_raw,
+                     desc_raw=desc_raw,
+                     kp_norm=kp_norm,
+                     desc_norm=desc_norm)
+
+
+def compute_descriptors_at_annotated_locations(
+        patch: SSSPatch,
+        algo: cv2.Feature2D,
+        kp_size: int = 16,
+        use_orig_sss_intensities: bool = False):
     """Compute traditional descriptors using OpenCV's cv2.Feature2D class for a given SSSPatch.
 
     Parameters
@@ -25,28 +61,38 @@ def compute_descriptors_at_annotated_locations(patch: SSSPatch, algo: cv2.Featur
 
     Returns
     -------
-    annotated_kps: list[cv2.KeyPoint]
-        The list of annotated keypoints, each keypoint is converted into a cv2.KeyPoint with
-        location at (bin_nbr (x value), ping_nbr (y value)), size = kp_size
+    annotated_kps: np.array
+        An array of annotated keypoints of shape (nbr_kps, 2).
+        Each array contains the (bin_nbr (x value), ping_nbr (y value)) of one keypoint.
     desc: np.array
         The descriptors computed at the annotated keypoint locations. Shape = (nbr_kps, 128)
     """
     annotated_kps = []
     for kp in patch.annotated_keypoints.values():
         ping_nbr, bin_nbr = kp['pos'][0], kp['pos'][1]
-        annotated_kps.append(cv2.KeyPoint(bin_nbr, ping_nbr, size=kp_size))
+        annotated_kps.append([bin_nbr, ping_nbr])
+
+    annotated_kps_as_cv2_kp = [
+        cv2.KeyPoint(bin_nbr, ping_nbr, size=kp_size)
+        for (bin_nbr, ping_nbr) in annotated_kps
+    ]
 
     img = patch.sss_waterfall_image
     if not use_orig_sss_intensities:
         img = normalize_waterfall_image(img)
 
-    normalized_8bit_img = cv2.normalize(img,  None,
-            0, 255, cv2.NORM_MINMAX).astype('uint8')
-    annotated_kps, desc = algo.compute(normalized_8bit_img, annotated_kps)
-    return annotated_kps, desc
+    normalized_8bit_img = cv2.normalize(img, None, 0, 255,
+                                        cv2.NORM_MINMAX).astype('uint8')
+    annotated_kps_as_cv2_kp, desc = algo.compute(normalized_8bit_img,
+                                                 annotated_kps_as_cv2_kp)
+    return np.array(annotated_kps, dtype=np.float64), desc
 
-def compute_flattened_neighbourhood_pixel_values(patch: SSSPatch, ping_neighbour: int = 8,
-        bin_neighbour: int = 8, use_orig_sss_intensities: bool = False):
+
+def compute_flattened_neighbourhood_pixel_values(
+        patch: SSSPatch,
+        ping_neighbour: int = 8,
+        bin_neighbour: int = 8,
+        use_orig_sss_intensities: bool = False):
     """For all annotated keypoints in a given SSSPatch, compute a descriptor in the form of flatten
     pixel values around the keypoint location. If the keypoint is at the edge, the neighbourhood
     that is outside the image will be filled with 0.
@@ -69,8 +115,9 @@ def compute_flattened_neighbourhood_pixel_values(patch: SSSPatch, ping_neighbour
 
     Returns
     -------
-    annotated_kps: list[(int, int)]
-        The list consist of tuples of the form (bin_nbr (x value), ping_nbr (y value)).
+    annotated_kps: np.array
+        An array of annotated keypoints of shape (nbr_kps, 2).
+        Each array contains the (bin_nbr (x value), ping_nbr (y value)) of one keypoint.
     desc: np.array
         The descriptors computed by flattening the pixel array defined by ping_neighbour and
         bin_neighbour. Shape = (nbr_kps, (ping_neighbour*2 + 1) * (bin_neighbour*2 + 1)).
@@ -80,9 +127,13 @@ def compute_flattened_neighbourhood_pixel_values(patch: SSSPatch, ping_neighbour
         img = normalize_waterfall_image(img)
 
     # Create a copy of the image so that all descriptors will be of the same size
-    pixel_vals = np.zeros((ping_neighbour*2 + img.shape[0], bin_neighbour*2 + img.shape[1]))
-    print(img.shape, pixel_vals[ping_neighbour:-ping_neighbour, bin_neighbour:-bin_neighbour].shape)
-    pixel_vals[ping_neighbour:-ping_neighbour, bin_neighbour:-bin_neighbour] = img
+    pixel_vals = np.zeros(
+        (ping_neighbour * 2 + img.shape[0], bin_neighbour * 2 + img.shape[1]))
+    print(
+        img.shape, pixel_vals[ping_neighbour:-ping_neighbour,
+                              bin_neighbour:-bin_neighbour].shape)
+    pixel_vals[ping_neighbour:-ping_neighbour,
+               bin_neighbour:-bin_neighbour] = img
 
     annotated_kps = []
     desc = []
@@ -90,19 +141,24 @@ def compute_flattened_neighbourhood_pixel_values(patch: SSSPatch, ping_neighbour
         ping_nbr, bin_nbr = kp['pos'][0], kp['pos'][1]
         annotated_kps.append((bin_nbr, ping_nbr))
 
-        flatten_pixels = pixel_vals[ping_nbr:ping_nbr + ping_neighbour*2 +1, bin_nbr:bin_nbr +
-                bin_neighbour*2 + 1].flatten()
+        flatten_pixels = pixel_vals[ping_nbr:ping_nbr + ping_neighbour * 2 + 1,
+                                    bin_nbr:bin_nbr + bin_neighbour * 2 +
+                                    1].flatten()
         desc.append(flatten_pixels)
         print(ping_nbr, bin_nbr, len(flatten_pixels))
-    return annotated_kps, np.array(desc)
+    return np.array(annotated_kps), np.array(desc)
 
-def draw_keypoints_and_descriptors(patch: SSSPatch, kps: list, desc: np.array, desc_name: str):
+
+def draw_keypoints_and_descriptors(patch: SSSPatch,
+                                   kps: np.array,
+                                   desc: np.array,
+                                   desc_name: str,
+                                   kp_size=16):
     #TODO: add documentation for this function
-    if isinstance(kps[0], tuple):
-        kp_size = desc.shape[1]//2
-        kps = [cv2.KeyPoint(x[0], x[1], size=kp_size) for x in kps]
-    img = cv2.normalize(normalize_waterfall_image(patch.sss_waterfall_image), None, 0, 255,
-            cv2.NORM_MINMAX).astype('uint8')
+    kps = [cv2.KeyPoint(x[0], x[1], size=kp_size) for x in kps]
+
+    img = cv2.normalize(normalize_waterfall_image(patch.sss_waterfall_image),
+                        None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
     kp_img = cv2.drawKeypoints(img, kps, None, color=(0, 255, 255))
 
     fig, ax = plt.subplots(1, 2)
@@ -111,7 +167,9 @@ def draw_keypoints_and_descriptors(patch: SSSPatch, kps: list, desc: np.array, d
     ax[1].imshow(desc)
     ax[1].set_title('Descriptors')
 
-    fig.suptitle(f'Keypoint and {desc_name} descriptors for patch {patch.patch_id}', fontsize=15)
+    fig.suptitle(
+        f'Keypoint and {desc_name} descriptors for patch {patch.patch_id}',
+        fontsize=15)
     fig.tight_layout()
     fig.subplots_adjust(top=1.05)
     return ax
