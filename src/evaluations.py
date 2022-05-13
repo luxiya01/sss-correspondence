562dc96bf3844c4ecd66c79b61992dc738724e9f
Li Ling <liling@kth.se>
2022-05-13 19:49:28 +0200

    Add matching score computation

--- src/evaluations.py
+++ src/evaluations.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def compute_precision_and_recall(gt: np.array, pred: np.array, prob: np.array,
+def compute_precision_recall_and_matching_score(gt: np.array, pred: np.array, prob: np.array,
                                  thresh: float):
     """Given the groundtruth, prediction, probability arrays and the probability threshold that is
-    used to count a prediction as a true match, returns the precision and recall of the
-    prediction. Assumes that keypoints without matches have gt = -1."""
+    used to count a prediction as a true match, returns the precision, recall and matching score of
+    the prediction. Assumes that keypoints without matches have gt = -1."""
     # Mask out all predictions with prob < thresh
     mask = np.where(prob < thresh)
     masked_pred = np.array(pred)
@@ -18,7 +18,8 @@ def compute_precision_and_recall(gt: np.array, pred: np.array, prob: np.array,
 
     precision = tp / (tp + fp)
     recall = tp / total_positive
+    matching_score = tp / len(gt) # num_correct / num_kps according to SuperGlue's implementation
 
     #    print(f'total positive: {total_positive}, tp: {tp}, fp: {fp}')
-    #    print(f'precision: {precision}, recall: {recall}')
-    return {'precision': precision, 'recall': recall}
+    #    print(f'precision: {precision}, recall: {recall}, matching_score: {matching_score}')
+    return {'precision': precision, 'recall': recall, 'matching_score': matching_score}
